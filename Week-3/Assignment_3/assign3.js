64c123f77fe0897ee893cfa4f1438cebc66d449b
RahulShende717 <rahulshende785@gmail.com>
2023-02-16 10:23:20 -0800

    some init msg

---
+++ Week-3/Assignment_3/assign3.js
@@ -0,0 +1,28 @@
+function createIncrement() { 
+    let count=0;
+    function increment() {
+        count++;
+    }
+    let message =`Count is ${count}`; 
+    function log() {
+        console.log(message);
+    }
+    return [increment, log];
+}
+
+const[increment,log] = createIncrement();
+
+increment();
+increment();
+increment();
+log();
+
+
+
+//the output of below problem will be 0
+
+/*beacause of the precedence of the functions execution. in the code variable "message  gets executes when we invoke 
+the "createIncrement();" and put 0 into the "count" variable then we invoke function "increment();" which increases the 
+value of "count" according the number of times we invoke the "increment();" function, then we invoke log() function to
+ print the "message" variable value which is "count is 0" and "count" having value 0 because "message" gets executed before
+  the invocation of "increment()" function. that is why variable "count"s value is 0.*/
